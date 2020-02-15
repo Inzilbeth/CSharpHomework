4049122b8c2c9b65d8f61e0fcedd5117a27db7a3
Inzilbeth <talgat.minkashev@yandex.com>
2020-02-15 19:31:21 +0300

    added task3

---
+++ Homework1/Task3/Homework1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework1._3
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            int array_size = 100;
+            int[] random_array = new int[array_size];
+            Random rand = new Random();
+            for (int i = 0; i < random_array.Length; i++)
+                random_array[i] = rand.Next(0,1000);
+            Array.Sort(random_array);
+            Console.WriteLine("Your sorted random array is: ");
+            for (int i=0;i< random_array.Length;i++)
+                Console.Write("{0} ",random_array[i]);
+        }
+    }
+}
