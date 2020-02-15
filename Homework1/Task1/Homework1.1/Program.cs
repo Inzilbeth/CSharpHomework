305911cfe8e06a2efbe1ad5ea626f8de954be583
Inzilbeth <talgat.minkashev@yandex.com>
2020-02-15 19:06:39 +0300

    added task 1 of homework 1

---
+++ Homework1/Task1/Homework1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Homework1._1
+{
+    class Program
+    {
+        private static int Factorial(int n)
+            => n <= 1 ? 1 : n * Factorial(n - 1);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial of 7 is: {0}",Factorial(7));
+        }
+    }
+}
