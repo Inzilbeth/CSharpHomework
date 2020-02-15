01772021921e4b608b4e3476af44bbab4083bb7f
Inzilbeth <talgat.minkashev@yandex.com>
2020-02-15 19:28:50 +0300

    added task2

---
+++ Homework1/Task2/Homework1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework1._2
+{
+    class Program
+    {
+        private static int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonachi(n - 1) + Fibonachi(n - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#7 Fibonachi number is {0}", Fibonachi(7));
+        }
+    }
+}
