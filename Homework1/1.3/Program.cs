bd8cf2b7fce1f7b078958afb351b6d1965db9e76
Inzilbeth <talgat.minkashev@yandex.com>
2020-02-17 22:58:03 +0300

    remove unneeded files, added input, added pre-sort array output

---
+++ Homework1/1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Homework1._3
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the random array: ");
+            int array_size = Convert.ToInt32(Console.ReadLine());
+            int[] random_array = new int[array_size];
+            Console.WriteLine("Your random array is: ");
+            Random rand = new Random();
+            for (int i = 0; i < random_array.Length; i++)
+            {
+                random_array[i] = rand.Next(0, array_size);
+                Console.Write($"{random_array[i]} ");
+            }
+            Array.Sort(random_array);
+            Console.WriteLine("\nYour sorted random array is: ");
+            for (int i = 0; i < random_array.Length; i++)
+                Console.Write("{0} ", random_array[i]);
+        }
+    }
+}
