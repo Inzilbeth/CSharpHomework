7e8d58e188d83c5f6c804018aa89dc3e6ff2f04a
Inzilbeth <talgat.minkashev@yandex.com>
2020-02-18 18:12:07 +0300

    style fix

--- Homework1/1.2/Program.cs
+++ Homework1/1.2/Program.cs
@@ -4,20 +4,6 @@ namespace Homework1._2
 {
     class Program
     {
-<<<<<<< Updated upstream
-        private static int Fibonachi(int n)
-        {
-            if (n == 0 || n == 1)
-            {
-                return n;
-            }
-            else
-            {
-                return Fibonachi(n - 1) + Fibonachi(n - 2);
-            }
-        }
-		
-=======
         public static int Fibonacci(int n)
         {
             int a = 0;
@@ -31,12 +17,11 @@ namespace Homework1._2
             return a;
         }
 
->>>>>>> Stashed changes
         static void Main(string[] args)
         {
             Console.Write("Enter value: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"#{number} Fibonachi number is {Fibonachi(number)}") ;
+            Console.WriteLine($"#{number} Fibonachi number is {Fibonacci(number)}") ;
         }
     }
 }
