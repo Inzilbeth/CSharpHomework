43f32b9d6d1fdf8ac000d69e1690714129d7783b
Inzilbeth <talgat.minkashev@yandex.com>
2020-02-18 18:07:40 +0300

    updated fibonacci method

--- Homework1/1.2/Program.cs
+++ Homework1/1.2/Program.cs
@@ -4,6 +4,7 @@ namespace Homework1._2
 {
     class Program
     {
+<<<<<<< Updated upstream
         private static int Fibonachi(int n)
         {
             if (n == 0 || n == 1)
@@ -16,6 +17,21 @@ namespace Homework1._2
             }
         }
 		
+=======
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+>>>>>>> Stashed changes
         static void Main(string[] args)
         {
             Console.Write("Enter value: ");
