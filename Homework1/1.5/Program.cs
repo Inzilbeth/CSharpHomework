12e372b2ac274784e6157f91be1d7fc10d36eb92
Inzilbeth <talgat.minkashev@yandex.com>
2020-02-23 13:15:33 +0300

    methods separation, style fixes

--- Homework1/1.5/Program.cs
+++ Homework1/1.5/Program.cs
@@ -5,49 +5,65 @@ namespace Task5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Sort2DArrsColumns(int[,] array)
         {
-            Console.WriteLine("Enter the size of the array: ");
-            int size = Convert.ToInt32(Console.ReadLine());
-            if(size<=0)
+            for (int i = 0; i < (Math.Sqrt(array.Length)); i++)
             {
-                Console.WriteLine("Entered value is inappropriate!");
-                return;
-            }
-            int[,] arrayToSort = new int[size, size];
-            Random rand = new Random();
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    arrayToSort[i, j] = rand.Next(0, size*size);
-            Console.WriteLine("Unsorted array: ");
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                    Console.Write($"{arrayToSort[i, j]} ");
-                Console.Write(Environment.NewLine);
-            }
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size - 1; j++)
+                for (int j = 0; j < (Math.Sqrt(array.Length) - 1); j++)
                 {
-                    if (arrayToSort[0, j] > arrayToSort[0, j + 1])
+                    if (array[0, j] > array[0, j + 1])
                     {
-                        for (int k = 0; k < size; k++)
+                        for (int k = 0; k < (Math.Sqrt(array.Length)); k++)
                         {
-                            int temporaryVariable = arrayToSort[k, j];
-                            arrayToSort[k, j] = arrayToSort[k, j + 1];
-                            arrayToSort[k, j + 1] = temporaryVariable;
+                            int temporaryVariable = array[k, j];
+                            array[k, j] = array[k, j + 1];
+                            array[k, j + 1] = temporaryVariable;
                         }
                     }
                 }
             }
-            Console.WriteLine("\nSorted array: ");
-            for (int i = 0; i < size; i++)
+        }
+
+        static void Print2DArray(int[,] array)
+        {
+            for (int i = 0; i < (Math.Sqrt(array.Length)); i++)
             {
-                for (int j = 0; j < size; j++)
-                    Console.Write($"{arrayToSort[i, j]} ");
+                for (int j = 0; j < (Math.Sqrt(array.Length)); j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
                 Console.Write(Environment.NewLine);
             }
+        }
+
+        static void Initialize2DArray(int[,] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < (Math.Sqrt(array.Length)) ; i++)
+            {
+                for (int j = 0; j < (Math.Sqrt(array.Length)); j++)
+                {
+                    array[i, j] = rand.Next(0, array.Length);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            if (size <= 0)
+            {
+                Console.WriteLine("Entered value is inappropriate!");
+                return;
+            }
+            var arrayToSort = new int[size, size];
+            Initialize2DArray(arrayToSort);
+            Console.WriteLine("Unsorted array: ");
+            Print2DArray(arrayToSort);
+            Sort2DArrsColumns(arrayToSort);
+            Console.WriteLine("\nSorted array: ");
+            Print2DArray(arrayToSort);
         }
     }
 }
